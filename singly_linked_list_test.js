61ecd87eea52f35d472ed030c9920ea0cc75d4a9
Gio d'Amelio <giodamelio@gmail.com>
2017-01-25 13:29:52 -0800

    Javascript: Add a few more tests for the linked list

--- singly_linked_list_test.js
+++ singly_linked_list_test.js
@@ -2,20 +2,32 @@ const SLL = require('./singly_linked_list');
 
 describe('Singly Linked List', function() {
   beforeEach(function() {
-    this.SLL = new SLL('A', 'B', 'C');
+    this.sll = new SLL('A', 'B', 'C');
   });
   
   it('Prepend a node', function() {
-    this.SLL.prepend('Z');
+    this.sll.prepend('Z');
 
-    this.SLL.head.value.should.equal('Z');
-    this.SLL.tail.value.should.equal('C');
+    this.sll.head.value.should.equal('Z');
+    this.sll.tail.value.should.equal('C');
+  });
+
+  it('Prepend to an empty linked list', function() {
+    const sll = new SLL();
+    sll.prepend('A');
+    sll.head.value.should.equal('A');
   });
 
   it('Append a node', function() {
-    this.SLL.append('Z');
+    this.sll.append('Z');
 
-    this.SLL.head.value.should.equal('A');
-    this.SLL.tail.value.should.equal('Z');
+    this.sll.head.value.should.equal('A');
+    this.sll.tail.value.should.equal('Z');
+  });
+
+  it('Append to an empty linked list', function() {
+    const sll = new SLL();
+    sll.append('A');
+    sll.head.value.should.equal('A');
   });
 });
