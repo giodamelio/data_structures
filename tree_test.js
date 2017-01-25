983b8df6322d9d4ac6c26a89d032e8dae4aa677c
Gio d'Amelio <giodamelio@gmail.com>
2017-01-25 13:55:54 -0800

    Javascript: Fix messages in tree_test.js

--- tree_test.js
+++ tree_test.js
@@ -1,14 +1,14 @@
 const Tree = require('./tree');
 
 describe('Tree', function() {
-  it('Should add a child', function() {
+  it('Add a child', function() {
     const root = new Tree('A');
     root.addChild('B');
     root.value.should.equal('A');
     root.children[0].value.should.equal('B');
   });
 
-  it('Should add multiple children', function() {
+  it('Add multiple children', function() {
     const root = new Tree('A');
     root.addChild('B', 'C');
     root.value.should.equal('A');
