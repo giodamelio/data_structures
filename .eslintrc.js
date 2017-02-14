27139d09df0ad24f0afd3b2535c16adf9e40760a
Gio d'Amelio <giodamelio@gmail.com>
2017-02-14 11:05:58 -0800

    Javascript: Setup linter

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: [
+    '@scottnonnenberg/thehelp/core',
+    '@scottnonnenberg/thehelp/es2015',
+    '@scottnonnenberg/thehelp/test',
+  ],
+  rules: {
+    'import/no-commonjs': 'off',
+    'max-statements': 'off',
+    'func-names': 'off',
+    'arrow-parens': ['error', 'always'],
+  }
+};
