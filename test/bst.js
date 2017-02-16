4d4f5c7daa293de6bd59ed1de423d62fc4e89d32
Gio d'Amelio <giodamelio@gmail.com>
2017-02-16 00:59:55 -0800

    Javascript: BST: add toDot function to convert tree to common .dot format

--- test/bst.js
+++ test/bst.js
@@ -45,3 +45,25 @@ ava.test('Push Many Item', (t) => {
   t.is(bst.root.right.right.value, 12);
 });
 
+ava.test('Empty tree', (t) => {
+  const bst = new BST();
+  t.is(bst.toDot(), 'graph {\n}\n');
+});
+
+ava.test('One node', (t) => {
+  const bst = new BST();
+  bst.insert(10);
+  t.is(bst.toDot(), 'graph {\n  10\n}\n');
+});
+
+ava.test('Multiple nodes', (t) => {
+  const bst = new BST();
+  bst.insert(10);
+  bst.insert(5);
+  bst.insert(15);
+  bst.insert(20);
+
+  // eslint-disable-next-line max-len
+  t.is(bst.toDot(), 'graph {\n  10 -- 5[color=red]\n  10 -- 15[color=green]\n  15 -- 20[color=green]\n  20\n  5\n}\n');
+});
+
